4786623382acdcde8e9c647ef6cc73610dc3151c
erickahmed <ahmederick00@gmail.com>
2022-08-10 23:20:09 +0200

    Update dependencies

--- dependencies.py
+++ dependencies.py
@@ -1,10 +1,12 @@
 import os
 
 # List of prerequisite packages needed
-os.system("pip install virtualenv")
 os.system("python -m pip install --upgrade pip")
+os.system("pip install scipy")
 os.system("pip install numpy")
+os.system("pip install virtualenv")
 os.system("pip install opencv-python")
 os.system("pip install pyautogui")
 os.system("pip install google-cloud")
 os.system("pip install google-cloud-vision")
+os.system("pip install pycopy-webbrowser")
